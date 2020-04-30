c6035b00f6c74d886ca08efbda01192c6cd7f854
Roman Wehmhöner <r.wehmhoner@gmail.com>
2020-05-01 01:01:06 +0200

    source added, formating repair,isBoolean

--- common.ts
+++ common.ts
@@ -1,20 +1,26 @@
 import * as fs from "fs";
 
+/**
+ * source https://stackoverflow.com/a/60203932
+ */
 export const readLineSync = () => {
   let rtnval = "";
 
   const buffer = Buffer.alloc ? Buffer.alloc(1) : new Buffer(1);
 
-  for(;;){
-      fs.readSync(0, buffer, 0, 1, undefined);   // 0 is fd for stdin
-      if(buffer[0] === 10){   // LF \n   return on line feed
-          break;
-      }else if(buffer[0] !== 13){     // CR \r   skip carriage return
-          rtnval += buffer.toString();
-      }
+  for (; ;) {
+    fs.readSync(0, buffer, 0, 1, undefined);   // 0 is fd for stdin
+    if (buffer[0] === 10) {   // LF \n   return on line feed
+      break;
+    } else if (buffer[0] !== 13) {   // CR \r   skip carriage return
+      rtnval += buffer.toString();
+    }
   }
 
   return rtnval;
 };
 
 
+export const isBoolean = (obj: any) => {
+  return obj === true || obj === false;
+};
