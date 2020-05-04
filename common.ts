238891fcf5d6e476967be06156b9f2060cc13056
Roman Wehmhöner <r.wehmhoner@gmail.com>
2020-05-04 23:30:16 +0200

    formatString

--- common.ts
+++ common.ts
@@ -33,6 +33,43 @@ export const readIntSync = (): number => {
 };
 
 
+export const formatString = (str: string, args: number[]): string => {
+  const split = str.split(/(%)([0-9]*)([idxXc])/);
+  let curI = 0;
+  while (curI < split.length && args.length) {
+    const curStr = split[curI];
+    if (curStr === "%") {
+      const padding = Number.parseInt(split[curI + 1], 10) || 0;
+      const style = split[curI + 2] as "i" | "d" | "x" | "X" | "c";
+      let formatedRes: string;
+      const num = args.pop();
+      switch (style) {
+        case "x":
+          formatedRes = num.toString(16);
+          break;
+        case "X":
+          formatedRes = num.toString(16);
+          formatedRes = formatedRes.toUpperCase();
+          break;
+        case "c":
+          formatedRes = String.fromCharCode(num);
+          break;
+        case "d":
+        case "i":
+          formatedRes = num.toString();
+          break;
+        default:
+          curI++;
+          continue;
+      }
+      formatedRes = formatedRes.padStart(padding, " ");
+      split.splice(curI, 3, formatedRes);
+    }
+    curI++;
+  }
+  return split.join("");
+};
+
 
 export const isBoolean = (obj: any) => {
   return obj === true || obj === false;
