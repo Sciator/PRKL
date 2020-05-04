dbf0056a831182e9fd8fb9a83536d5ed23d24a21
Roman Wehmhöner <r.wehmhoner@gmail.com>
2020-05-04 23:31:00 +0200

    _

--- index.ts
+++ index.ts
@@ -1,4 +1,15 @@
 import { mCExecutinTreeGenerator } from "./mCExecutinTreeGenerator";
+import { writeFile } from "fs";
+import { promisify } from "util";
+import { Interpreter } from "./Intrerpreter";
 
 
-mCExecutinTreeGenerator.run();
+(async () => {
+  const tree = mCExecutinTreeGenerator.run();
+
+  await promisify(writeFile)("./output.json", JSON.stringify(tree, undefined, 2));
+
+  new Interpreter(tree).start();
+})();
+
+
